d369b646740a2378104cf438946a2af31364d694
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-10 12:40:01 -0400

    test: add pgxmock package, add CreateQuery unit test

--- test/unit/database/query_test.go
+++ test/unit/database/query_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/muzzarellimj/grace-material-api/database"
+	"github.com/pashagolub/pgxmock/v3"
 )
 
 func TestCreateQuerySelectionFrom(t *testing.T) {
@@ -43,5 +44,30 @@ func TestCreateQuerySelectionFromWhereGroupDirective(t *testing.T) {
 
 	if query != expected || err != nil {
 		t.Fatalf("Expected query statement %s does not match actual query statement %s: %v\n", expected, query, err)
+	}
+}
+
+func TestExecuteQuerySelects(t *testing.T) {
+	mock, err := pgxmock.NewPool()
+
+	if err != nil {
+		t.Fatalf("Unable to create mock database connection pool: %v\n", err)
+	}
+
+	defer mock.Close()
+
+	mock.ExpectQuery("SELECT id FROM example WHERE id=1").WillReturnRows(pgxmock.NewRows([]string{"1"}))
+
+	statement, _ := database.CreateQuery("id", "example", "id=1", "")
+	_, err = database.ExecuteQuery(mock, statement)
+
+	if err != nil {
+		t.Fatalf("Query statement '%s' was unable to execute with success: %v\n", statement, err)
+	}
+
+	err = mock.ExpectationsWereMet()
+
+	if err != nil {
+		t.Fatalf("Query statement '%s' execution was unable to meet mock expectations: %v\n", statement, err)
 	}
 }
