e680d203632cef29ead0d6114cc9fd9877833c73
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-12 22:59:17 -0400

    feat: revise internal movie model pattern

--- model/internal-movie.go
+++ model/internal-movie.go
@@ -1,28 +1,36 @@
 package model
 
-import "time"
-
 type Movie struct {
-	ID                  int       `json:"id"`
-	Title               string    `json:"title"`
-	Description         *string   `json:"description"`
-	Tagline             *string   `json:"tagline"`
-	Genres              *[]string `json:"genres"`
-	ProductionCompanies *[]string `json:"production_companies"`
-	ReleaseDate         time.Time `json:"release_date"`
-	Runtime             *int      `json:"runtime"`
-	Image               *string   `json:"image"`
-	ReferenceImdb       string    `json:"reference_imdb"`
-	ReferenceTmdb       string    `json:"reference_tmdb"`
+	ID                  int                          `json:"id"`
+	Title               string                       `json:"title"`
+	Tagline             string                       `json:"tagline"`
+	Description         string                       `json:"description"`
+	Genres              []MGenreFragment             `json:"genres"`
+	ProductionCompanies []MProductionCompanyFragment `json:"production_companies"`
+	ReleaseDate         string                       `json:"release_date"`
+	Runtime             int                          `json:"runtime"`
+	Image               string                       `json:"image"`
+	Reference           int                          `json:"reference"`
 }
 
-type MovieGenre struct {
+type MovieFragment struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Tagline     string `json:"tagline"`
+	Description string `json:"description"`
+	ReleaseDate string `json:"release_date"`
+	Runtime     int    `json:"runtime"`
+	Image       string `json:"image"`
+	Reference   int    `json:"reference"`
+}
+
+type MGenreFragment struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
 	Reference int    `json:"reference"`
 }
 
-type MovieProductionCompany struct {
+type MProductionCompanyFragment struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
 	Image     string `json:"image"`
