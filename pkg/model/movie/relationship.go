d7c3308f792971652faf71ec856e15977f7e4182
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-13 21:21:18 -0400

    fix: revise movie and genre as int, not string

--- pkg/model/movie/relationship.go
+++ pkg/model/movie/relationship.go
@@ -1,13 +1,13 @@
 package model
 
 type MovieGenreRelationship struct {
-	ID    int    `json:"id"`
-	Movie string `json:"movie"`
-	Genre string `json:"genre"`
+	ID    int `json:"id"`
+	Movie int `json:"movie"`
+	Genre int `json:"genre"`
 }
 
 type MovieProductionCompanyRelationship struct {
-	ID                int    `json:"id"`
-	Movie             string `json:"movie"`
-	ProductionCompany string `json:"production_company"`
+	ID                int `json:"id"`
+	Movie             int `json:"movie"`
+	ProductionCompany int `json:"production_company"`
 }
