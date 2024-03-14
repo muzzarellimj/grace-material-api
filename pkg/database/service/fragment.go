40f8d001c1b23377043218745d1e51968076f32c
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-13 22:05:33 -0400

    feat: create function to fetch single fragment

--- pkg/database/service/fragment.go
+++ pkg/database/service/fragment.go
@@ -7,6 +7,24 @@ import (
 	"github.com/muzzarellimj/grace-material-api/database"
 )
 
+func FetchFragment[M interface{}](connection database.PgxConnection, table string, constraint string) (M, error) {
+	var zero M
+
+	fragmentSlice, err := FetchFragmentSlice[M](connection, table, constraint)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to fetch initial fragment slice: %v\n", err)
+
+		return zero, err
+	}
+
+	if len(fragmentSlice) > 0 {
+		return fragmentSlice[0], nil
+	}
+
+	return zero, nil
+}
+
 func FetchFragmentSlice[M interface{}](connection database.PgxConnection, table string, constraint string) ([]M, error) {
 	statement, err := database.CreateQuery("*", table, constraint, "")
 
