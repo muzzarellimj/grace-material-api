a72266ed7b6ca518742dcddfe2031555ccf722d7
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-16 16:26:34 -0400

    feat: create StoreRelationship function as StoreFragment counterpart

--- pkg/database/service/relationship.go
+++ pkg/database/service/relationship.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/muzzarellimj/grace-material-api/pkg/database/connection"
 )
 
@@ -37,4 +40,45 @@ func FetchRelationshipSlice[M interface{}](connection connection.PgxPool, table 
 	}
 
 	return relationshipSlice, nil
+}
+
+// Store a relationship in the provided table with the provided properties (column names) and named arguments.
+//
+// Return: nil with success, and error without.
+func StoreRelationship(connection connection.PgxPool, table string, properties []string, arguments pgx.NamedArgs) error {
+	var names []string
+
+	for _, property := range properties {
+		names = append(names, fmt.Sprint("@", property))
+	}
+
+	statement := fmt.Sprintf("INSERT INTO %s (%v) VALUES (%v)", table, strings.Join(properties, ","), strings.Join(names, ","))
+
+	tx, err := connection.Begin(context.Background())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to begin transaction to store relationship: %v\n", err)
+
+		return err
+	}
+
+	defer tx.Rollback(context.Background())
+
+	_, err = tx.Exec(context.Background(), statement, arguments)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to execute relationship insertion statement: %v\n", err)
+
+		return err
+	}
+
+	err = tx.Commit(context.Background())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to commit relationship insertion transaction: %v\n", err)
+
+		return err
+	}
+
+	return nil
 }
