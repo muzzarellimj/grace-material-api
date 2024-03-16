9755fcb5fbc90761c2206345469483d8ab4dc88a
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-16 07:45:22 -0400

    feat: create TMDB API movie detail handler

---
+++ pkg/api/third_party/themoviedb.org/api.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	model "github.com/muzzarellimj/grace-material-api/pkg/model/third_party/themoviedb.org"
+	"github.com/muzzarellimj/grace-material-api/pkg/util"
+)
+
+const (
+	TMDBBase          = "https://api.themoviedb.org/3"
+	TMDBEndpointMovie = "/movie/"
+)
+
+// Get the top-level details of a movie with a provided numeric identifier.
+//
+// Return: decoded movie detail response and nil with success, empty movie detail response and error without.
+func GetMovieDetail(id int) (model.TMDBMovieDetailResponse, error) {
+	path, err := util.CreateRequestPath(TMDBBase, TMDBEndpointMovie, fmt.Sprint(id), map[string]string{})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create request path to '%s%s': %v\n", TMDBBase, TMDBEndpointMovie, err)
+
+		return model.TMDBMovieDetailResponse{}, err
+	}
+
+	request, err := util.CreateRequest(http.MethodGet, path, map[string]string{"Authorization": fmt.Sprint("Bearer ", os.Getenv("TMDB_API_KEY"))})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create '%s' request to '%s': %v\n", http.MethodGet, path, err)
+
+		return model.TMDBMovieDetailResponse{}, err
+	}
+
+	response, err := util.ExecuteRequest(request)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to execute '%s' request to '%s': %v\n", request.Method, request.URL.String(), err)
+
+		return model.TMDBMovieDetailResponse{}, err
+	}
+
+	var movie model.TMDBMovieDetailResponse
+
+	err = json.NewDecoder(response.Body).Decode(&movie)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to decode response as movie detail model: %v\n", err)
+
+		return model.TMDBMovieDetailResponse{}, err
+	}
+
+	return movie, nil
+}
