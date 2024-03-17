e87bf3f6128dd451619454ef9f19dac57e1aa11d
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-17 15:45:23 -0400

    feat: create table, properties database constants

--- pkg/database/constant.go
+++ pkg/database/constant.go
@@ -2,6 +2,24 @@ package database
 
 // Table names, which may be shared between databases.
 const (
+	TableBookFragments              = "books"
+	TableBookAuthorFragments        = "authors"
+	TableBookPublisherFragments     = "publishers"
+	TableBookTopicFragments         = "topics"
+	TableBookAuthorRelationships    = "books_authors"
+	TableBookPublisherRelationships = "books_publishers"
+	TableBookTopicRelationships     = "books_topics"
+
+	TableGameFragments              = "games"
+	TableGameFranchiseFragments     = "franchises"
+	TableGameGenreFragments         = "genres"
+	TableGamePlatformFragments      = "platforms"
+	TableGameStudioFragments        = "studios"
+	TableGameFranchiseRelationships = "games_franchises"
+	TableGameGenreRelationships     = "games_genres"
+	TableGamePlatformRelationships  = "games_platforms"
+	TableGameStudioRelationships    = "games_studios"
+
 	TableMovieFragments                      = "movies"
 	TableMovieGenreFragments                 = "genres"
 	TableMovieProductionCompanyFragments     = "production_companies"
@@ -11,6 +29,24 @@ const (
 
 // Properties (or columns names) per database table.
 var (
+	PropertiesBookFragments              = []string{"id", "title", "subtitle", "description", "publish_date", "pages", "isbn10", "isbn13", "image", "edition_reference", "work_reference"}
+	PropertiesBookAuthorFragments        = []string{"id", "firstname", "middlename", "lastname", "biography", "image", "reference"}
+	PropertiesBookPublisherFragments     = []string{"id", "name"}
+	PropertiesBookTopicFragments         = []string{"id", "name"}
+	PropertiesBookAuthorRelationships    = []string{"book", "author"}
+	PropertiesBookPublisherRelationships = []string{"book", "publisher"}
+	PropertiesBookTopicRelationships     = []string{"book", "topic"}
+
+	PropertiesGameFragments              = []string{"id", "title", "summary", "storyline", "release_date", "image", "reference"}
+	PropertiesGameFranchiseFragments     = []string{"id", "name", "reference"}
+	PropertiesGameGenreFragments         = []string{"id", "name", "reference"}
+	PropertiesGamePlatformFragments      = []string{"id", "name", "reference"}
+	PropertiesGameStudioFragments        = []string{"id", "name", "description", "reference"}
+	PropertiesGameFranchiseRelationships = []string{"game", "franchise"}
+	PropertiesGameGenreRelationships     = []string{"game", "genre"}
+	PropertiesGamePlatformRelationships  = []string{"game", "platform"}
+	PropertiesGameStudioRelationships    = []string{"game", "studio"}
+
 	PropertiesMovieFragments                      = []string{"title", "tagline", "description", "release_date", "runtime", "image", "reference"}
 	PropertiesMovieGenreFragments                 = []string{"name", "reference"}
 	PropertiesMovieProductionCompanyFragments     = []string{"name", "image", "reference"}
