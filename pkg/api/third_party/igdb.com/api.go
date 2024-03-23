06b23ae8fef4a2e70a7ba2566f3a79be5ef57886
Michael Muzzarelli <muzzarellimj@outlook.com>
2024-03-23 00:36:05 -0400

    feat: support IGDB resource and resource slice fetching

--- pkg/api/third_party/igdb.com/api.go
+++ pkg/api/third_party/igdb.com/api.go
@@ -19,11 +19,11 @@ const (
 	IGDBEndpointPlatform        = "/v4/platforms"
 )
 
-// Get an IGDB resource with a provided model and Apicalypse-compliant constraint.
+// Get an IGDB resource slice with a provided model to decode to and an Apicalypse-compliant constraint.
 //
-// Return: decoded game model and nil with success, empty game response and error without.
-func IGDBGetResource[M interface{}](endpoint string, constraint string) (M, error) {
-	var zero M
+// Return: decoded model slice and nil with success, empty model slice and error without.
+func IGDBGetResourceSlice[M interface{}](endpoint string, constraint string) ([]M, error) {
+	var zero []M
 
 	path, err := util.CreateRequestPath(os.Getenv("AWS_PROXY_HOST"), endpoint, "", map[string]string{})
 
@@ -57,6 +57,23 @@ func IGDBGetResource[M interface{}](endpoint string, constraint string) (M, erro
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to decode response to provided resource model: %v\n", err)
+
+		return zero, err
+	}
+
+	return resourceSlice, nil
+}
+
+// Get one IGDB resource with a provided model to decode to and an Apicalyps-compliant constraint.
+//
+// Return: decoded model and nil with success, empty model and error without.
+func IGDBGetResource[M interface{}](endpoint string, constraint string) (M, error) {
+	var zero M
+
+	resourceSlice, err := IGDBGetResourceSlice[M](endpoint, constraint)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to get and decode initial resource slice: %v\n", err)
 
 		return zero, err
 	}
